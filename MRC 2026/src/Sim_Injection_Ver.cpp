18a9a9fcef006bb6e3816570fbde442296070038
Sivi2706 <137089663+Sivi2706@users.noreply.github.com>
2026-09-06 21:51:13 +0800

    Update Sim_Injection_Ver.cpp

--- MRC 2026/src/Sim_Injection_Ver.cpp
+++ MRC 2026/src/Sim_Injection_Ver.cpp
@@ -70,7 +70,7 @@
 #define CO2_EJECTION_SERVO_PIN     27
 
 // Servo positions in degrees. Change these values later to match the actual
-// mechanical linkage. For the CanSat doors, 180° = CLOSED and 90° = OPEN.
+// mechanical linkage. For the CanSat doors, 180° = CLOSED and 90° = OPEN.huohou;ou;ouh
 static const int CANSAT_TOP_INITIAL_ANGLE_DEG      = 180;
 static const int CANSAT_TOP_OPEN_ANGLE_DEG         = 90;
 static const int CANSAT_BOTTOM_INITIAL_ANGLE_DEG   = 180;
